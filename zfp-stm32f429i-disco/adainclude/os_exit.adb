74bbce1fc387ce8af5ba76d1efb8155b2003e34d
Simon Wright <simonjwright@users.sourceforge.net>
2015-01-23 14:32:12 +0000

    Removed unused zfp directories.

--- zfp-stm32f429i-disco/adainclude/os_exit.adb
+++
@@ -1,7 +0,0 @@
-procedure OS_Exit (Code : Integer) is
-   pragma Unreferenced (Code);
-begin
-   loop
-      null;
-   end loop;
-end OS_Exit;
--- zfp-stm32f429i-disco/adainclude/os_exit.ads
+++
@@ -1,3 +0,0 @@
-procedure OS_Exit (Code : Integer);
-pragma Export (C, OS_Exit, "exit");
-pragma No_Return (OS_Exit);
