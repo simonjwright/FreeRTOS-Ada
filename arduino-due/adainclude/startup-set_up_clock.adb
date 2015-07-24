6f14f374a8957dffe844fd03f8b46de909855f7c
Simon Wright <simonjwright@users.sourceforge.net>
2015-07-24 10:49:14 +0100

    More on clock setup. Still crashing with PLLA.
      * arduino-due/adainclude/startup-set_up_clock.adb: copied code
          sequence from 11057 ch 28.12. Didn't work, so left PLLA section
          commented out.

--- arduino-due/adainclude/startup-set_up_clock.adb
+++ arduino-due/adainclude/startup-set_up_clock.adb
@@ -3,6 +3,8 @@ with Interfaces;
 separate (Startup)
 procedure Set_Up_Clock is
 
+   --  See Programming Sequence, 11507 23-Mar-15, Chapter 28.12
+
    CKGR_MOR : Interfaces.Unsigned_32
      with
        Import,
@@ -38,7 +40,15 @@ procedure Set_Up_Clock is
        Volatile,
        Address => System'To_Address (16#400E0630#);
 
+   MCKR : Interfaces.Unsigned_32;
+
    use type Interfaces.Unsigned_32;
+
+   MOR_Loops : Interfaces.Unsigned_32 := 0 with Volatile;
+   MO_Loops : Interfaces.Unsigned_32 := 0 with Volatile;
+   PLLA_Loops : Interfaces.Unsigned_32 := 0 with Volatile;
+   MC_Loops_CSS : Interfaces.Unsigned_32 := 0 with Volatile;
+   MC_Loops_PRES : Interfaces.Unsigned_32 := 0 with Volatile;
 begin
 
    --  Write CKGR_MOR to select the Main Clock
@@ -51,6 +61,7 @@ begin
    --  Loop until stable - check MOSCXTS, bit 0
    loop
       exit when (PMC_SR and 16#00000001#) /= 0;
+      MOR_Loops := MOR_Loops + 1;
    end loop;
 
    --  Select the Main oscillator
@@ -61,12 +72,13 @@ begin
    --  Loop until selected - check MOSCSELS, bit 16, is 0 - or not???
    loop
       exit when (PMC_SR and 16#00010000#) /= 0;
+      MO_Loops := MO_Loops + 1;
    end loop;
 
    --  Disable PLLA (?hardware bugfix?)
    CKGR_PLLAR :=
      16#20000000# or  -- ONE
-     16#00000000#;    -- MULA = 0, disable
+     16#00000000#;    -- MULA = 0, DIVA = 0, => disable
 
    --  Set PLLA to multiply by 14, count 16#3f#, divide by 1 (=>
    --  enable PLL); Main Clock is 12 MHz, => 168 Mhz
@@ -79,34 +91,43 @@ begin
    --  Loop until ready - check LOCKA, bit 1
    loop
       exit when (PMC_SR and 16#00000002#) /= 0;
+      PLLA_Loops := PLLA_Loops + 1;
    end loop;
 
-   --  --  Select PLLA as clock source, prescale by 2, no division
-   --  PMC_MCKR :=
-   --    16#00000010# or  -- PRES
-   --    16#00000002#;    -- PLLA_CLK
-
    --  Select Main Clock, PRES 0 (no prescaling)
-   PMC_MCKR := 16#00000001#;
-
+   --  Set CSS
+   MCKR := PMC_MCKR and (not 16#0000000f#);
+   PMC_MCKR := MCKR or 16#00000001#;
    --  Loop until ready - check MCKRDY, bit 3
    loop
       exit when (PMC_SR and 16#00000008#) /= 0;
+      MC_Loops_CSS := MC_Loops_CSS + 1;
+   end loop;
+   --  Set PRES
+   MCKR := PMC_MCKR and (not 16#00000030#);
+   PMC_MCKR := MCKR;
+   --  Loop until ready - check MCKRDY, bit 3
+   loop
+      exit when (PMC_SR and 16#00000008#) /= 0;
+      MC_Loops_PRES := MC_Loops_PRES + 1;
    end loop;
 
-   --  --  Select PRES to 1 (=> prescale by 2), still with main clock
-   --  PMC_MCKR := 16#00000011#;
-
+   --  --  Select PLLA with PRES=1 (=> prescale by 2); other way round from
+   --  --  Main_Clock above, as recommended
+   --  --  Set PRES
+   --  MCKR := PMC_MCKR and (not 16#00000030#);
+   --  PMC_MCKR := MCKR or 16#00000010#;
    --  --  Loop until ready - check MCKRDY, bit 3
    --  loop
    --     exit when (PMC_SR and 16#00000008#) /= 0;
+   --     MC_Loops_PRES := MC_Loops_PRES + 1;
    --  end loop;
-
-   --  --  Select PLLA, still with PRES to 1
-   --  PMC_MCKR := 16#00000012#;
-
+   --  --  Set CSS
+   --  MCKR := PMC_MCKR and (not 16#0000000f#);
+   --  PMC_MCKR := MCKR or 16#00000002#;
    --  --  Loop until ready - check MCKRDY, bit 3
    --  loop
    --     exit when (PMC_SR and 16#00000008#) /= 0;
+   --     MC_Loops_CSS := MC_Loops_CSS + 1;
    --  end loop;
 end Set_Up_Clock;
