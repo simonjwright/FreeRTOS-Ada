77fac004616602f91f630e5d76c420704ebd6304
Simon Wright <simonjwright@users.sourceforge.net>
2015-08-20 17:50:00 +0100

    Started on Arduino BSP, with potential common BSP code separated.
      * arduino-due-bsp/arduino_due_bsp.gpr: new.
      * arduino-due-bsp/adainclude/registers-atsam3x8-peripheral_identifiers.ads:
          moved from test-arduino-due/.
      * arduino-due-bsp/adainclude/registers-atsam3x8-pio.ads: likewise.
      * arduino-due-bsp/adainclude/registers-atsam3x8-pmc.ads: likewise.
      * arduino-due-bsp/adainclude/registers-atsam3x8.ads: likewise.
    
      * bsp-common/registers-cortex-systick.ads: moved from test-arduino-due/.
      * bsp-common/registers-cortex.ads: likewise.
      * bsp-common/registers.ads: likewise.
      * bsp-common/registers-cortex-nvic.ads: likewise.
        (NVIC_T): corrected offsets.
    
      * test-arduino-due/testbed.gpr (context): with arduino_due_bsp.
        (Builder): add -ffunction-sections, -fdata-sections.
        (Linker): add -gc-sections.

--- bsp-common/registers-cortex-nvic.ads
+++ bsp-common/registers-cortex-nvic.ads
@@ -15,16 +15,18 @@ package Registers.Cortex.NVIC with Preelaborate is
       ICPR : Bits_32x1_Array (0 .. 7);
       IABR : Bits_32x1_Array (0 .. 7);
       IPRO : Bits_32x4_Array (0 .. 59);
+      STIR : Interfaces.Unsigned_32;
    end record
-   with Convention => Ada, Volatile, Size => 8 * (16#04EC# + 4);
+   with Convention => Ada, Volatile, Size => 8 * (16#0E00# + 4);
 
    for NVIC_T use record
       ISER at 16#0000# range 0 .. 255;
-      ICER at 16#0180# range 0 .. 255;
-      ISPR at 16#0200# range 0 .. 255;
-      ICPR at 16#0280# range 0 .. 255;
-      IABR at 16#0300# range 0 .. 255;
-      IPRO at 16#0400# range 0 .. 1919;
+      ICER at 16#0080# range 0 .. 255;
+      ISPR at 16#0100# range 0 .. 255;
+      ICPR at 16#0180# range 0 .. 255;
+      IABR at 16#0200# range 0 .. 255;
+      IPRO at 16#0300# range 0 .. 1919;
+      STIR at 16#0E00# range 0 .. 31;
    end record;
 
    NVIC : NVIC_T
