b2ebf0131831c4f46ee0d8ab101ca0167247df82
Simon Wright <simon@pushface.org>
2017-08-31 17:48:33 +0100

    Corrected unrecognised pragma.
      * common/a-strmap.ads: replaced pragmas Elaborate, Pure_05 (not
          recognised by GCC 7.1.0) with Pure.

--- common/a-strmap.ads
+++ common/a-strmap.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---          Copyright (C) 1992-2009, Free Software Foundation, Inc.         --
+--       Copyright (C) 1992-2009, 2017, Free Software Foundation, Inc.      --
 --                                                                          --
 -- This specification is derived from the Ada Reference Manual for use with --
 -- GNAT. The copyright notice above, and the license provisions that follow --
@@ -37,7 +37,6 @@ with Ada.Characters.Latin_1;
 
 package Ada.Strings.Maps is
    pragma Preelaborate;
-   pragma Pure_05;
    --  In accordance with Ada 2005 AI-362
 
    --------------------------------
