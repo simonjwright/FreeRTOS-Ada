ed88765faf1fd5400ff7ec5cb9e1da6541a5e45c
Simon Wright <simon@pushface.org>
2020-10-13 17:55:26 +0100

    Raise PE on potentially-blocking operation in PO.
      * common/a-retide.adb (Delay_Until): raise PE if within a PO.
      * common/s-taprob.adb: updated comments to note that this is a
          restricted profile, so Detect_Blocking can be assumed, even absent
          the pragma.
      * test-stm32f4/delay_in_po-main.adb, test-stm32f4/delay_in_po.adb,
        test-stm32f4/delay_in_po.ads: demonstrate that blocking is detected.
      * test-stm32f4/testbed.gpr: add delay_in_po test.

---
+++ test-stm32f4/delay_in_po.adb
@@ -0,0 +1,23 @@
+--  Copyright (C) 2020 Free Software Foundation, Inc.
+
+--  This file is part of the Cortex GNAT RTS package.
+--
+--  Copying and distribution of this file, with or without
+--  modification, are permitted in any medium without royalty provided
+--  the copyright notice and this notice are preserved.  This file is
+--  offered as-is, without any warranty.
+
+with Ada.Real_Time;
+
+package body Delay_In_PO is
+
+   protected body PO is
+
+      procedure Potentially_Blocking is
+      begin
+         delay until Ada.Real_Time.Time_Last;
+      end Potentially_Blocking;
+
+   end PO;
+
+end Delay_In_PO;
